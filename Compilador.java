2870bb8468386b7eae0e684522e66ffee2513010
Matheus <matheustalves@outlook.com>
2021-08-31 14:27:39 -0300

    added states

--- Compilador.java
+++ Compilador.java
@@ -2,8 +2,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Hashtable;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Compilador {
+    static Hashtable<String, Symbol> symbolTable = new Hashtable<String, Symbol>();
+    static List<Token> tokenList = new ArrayList<Token>();
     static final String[] reservedWords = { "const", "int", "char", "while", "if", "float", "else", "&&", "||", "!",
             "<-", "=", "(", ")", "<", ">", "!=", ">=", "<=", ",", "+", "-", "*", "/", ";", "{", "}", "readln", "div",
             "write", "writeln", "mod", "[", "]" };
@@ -54,11 +58,17 @@ public class Compilador {
         }
     }
 
-    class Token {
-        int num;
-        String lexema;
-        String tipo;
-        byte tamanho;
+    static class Token {
+        int token;
+        String lexeme;
+        String type;
+        byte bsize;
+
+        Token(String lex, int tk, String t) {
+            lexeme = lex;
+            token = tk;
+            type = t;
+        }
     }
 
     static class Lexer {
@@ -68,42 +78,113 @@ public class Compilador {
             int nextState = 0;
             lexeme += c;
 
-            if (Character.isLetter(c)) {
+            if (Character.isLetter(c) || c == '_') {
                 nextState = 1;
-            } else if (c == '.' || c == '_') {
+            } else if (c == '<') {
                 nextState = 2;
+            } else if (Character.isDigit(c)) {
+                nextState = 3;
+            } else if (c == ';') {
+                nextState = 14;
             }
             return nextState;
         }
 
         int state1(char c) {
             int nextState = 0;
+            lexeme += c;
+
+            if (Character.isLetter(c) || Character.isDigit(c) || c == '.' || c == '_') {
+                nextState = 1;
+            } else {
+                nextState = 0;
+
+                Symbol symbol = symbolTable.get(lexeme);
+
+                if (symbol != null) {
+                    Token token = new Token(lexeme, symbol.token, "String");
+                    tokenList.add(token);
+                } else { // is id
+                    Symbol newSymbol = new Symbol(lexeme, tokenId);
+                    Token token = new Token(lexeme, tokenId, "String");
+
+                    symbolTable.put(lexeme, newSymbol);
+                    tokenList.add(token);
+                }
+            }
             return nextState;
         }
 
-        void nextState(char c) {
-            int currentState = 0;
+        int state2(char c) {
+            int nextState = 0;
+            lexeme += c;
 
-            while (currentState != 15) {
-                switch (currentState) {
-                    case 0:
-                        currentState = state0(c);
-                        break;
-                }
+            if (c == '-') {
+                nextState = 0;
             }
+            return nextState;
         }
 
-        String getLexeme(String str) {
-            for (int i = 0; i < str.length(); i++) {
-                nextState(str.charAt(i));
+        int state3(char c) {
+            int nextState = 0;
+            lexeme += c;
+
+            if (Character.isDigit(c)) {
+                nextState = 3;
+            } else if (c == '.') {
+                nextState = 4;
             }
+            return nextState;
+        }
+
+        // int state14(char c) {
+        // int nextState = 0;
+        // lexeme += c;
+
+        // if (Character.isDigit(c)) {
+        // nextState = 3;
+        // }else if(c == '.'){
+        // nextState = 4;
+        // }
+        // return nextState;
+        // }
+
+        String getLexeme(String str) {
+            int currentState = 0;
+            int i = 0;
+            char c;
+
+            while (currentState != 15) {
+                if (i < str.length()) {
+                    c = str.charAt(i);
+                    i++;
+
+                    switch (currentState) {
+                        case 0:
+                            currentState = state0(c);
+                            break;
+                        case 1:
+                            currentState = state1(c);
+                            break;
+                        case 2:
+                            currentState = state2(c);
+                            break;
+                        case 3:
+                            currentState = state3(c);
+                            break;
+                        case 15:
+                            break;
+                    }
+                } else {
+                }
+
+            }
+
             return lexeme;
         }
     }
 
     public static void main(String[] args) {
-        Hashtable<String, Symbol> symbolTable = new Hashtable<String, Symbol>();
-
         for (int i = 0; i < reservedWords.length; i++) {
             Symbol symbol = new Symbol(reservedWords[i], i + 2);
             symbolTable.put(reservedWords[i], symbol);
@@ -112,8 +193,8 @@ public class Compilador {
         try {
             File file = new File("programa.txt");
             Scanner scanner = new Scanner(file);
+            Lexer lexer = new Lexer();
             while (scanner.hasNext()) {
-                Lexer lexer = new Lexer();
                 String str = lexer.getLexeme(scanner.next());
                 System.out.println(str);
             }
